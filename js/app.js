d9629795e3b01d7dd0e379fc96b2ccd8f0a7ee63
Mackenzie <clarkmackenzieb@gmail.com>
2017-09-12 23:54:52 -0500

    WE GOT THE CHARTS WE DID IT REDDIT

--- js/app.js
+++ js/app.js
@@ -8,7 +8,85 @@ angular.module("budgetApp", ['ui.router'])
     })
     .state('income',{
         url:"/income",
-        templateUrl:"../views/income.html"
+        templateUrl:"../views/income.html",
+        controller: function($scope){
+            var myChart = document.getElementById('myChart').getContext('2d')
+            Chart.defaults.global.defaultFontFamily =  'Lato';
+            Chart.defaults.global.defaultFontSize = 25;
+            Chart.defaults.global.defaultFontColor = '#777';
+            var lineChart = new Chart(myChart, {
+              type: 'line',
+              data: {
+                labels:[2008,2009,2010,2011,2012,2013,2014,2015],
+                datasets: [{
+                  label: "Satisfaction in Points",
+                  fill: false,
+                  lineTension: 0.1,
+                  borderColor: "blue",
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: 'red',
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: 'pink',
+                  pointHoverBorderColor: 'red',
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: [5.7,5.8,5.9,6.0,6.1,6.2,6.3,6.4],
+                  // backgroundColor: 'green'
+                  backgroundColor: [
+                    'rgba(225,226,228,0.5)'
+                  ]
+                }]
+              },
+              options: {
+                responsive: true,
+                scaleShowValues: true,
+                scales: {
+                  yAxes: [{
+                    ticks: {
+                      beginAtZero: true,
+                      steps: 0.5,
+                      stepValue: 0.1,
+                      max: 6.4,
+                      min: 5.7
+                    }
+                  }],
+                  xAxes: [{
+                    ticks: {
+                      autoSkip: false
+                    }
+                  }]
+              },
+                title: {
+                  display: true,
+                  text: 'Happiness Levels Measured',
+                  fontSize: 25
+                },
+                legend: {
+                  display: true,
+                  position: 'right',
+                  labels: {
+            
+                 }
+                },
+                layout: {
+                  padding: {
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top: 0
+                  }
+                },
+                tooltips:{
+                  enabled: true
+                }
+              }
+            })
+        }
     })
     .state('budget',{
         url:"/budget",
--- js/controller.js
+++ js/controller.js
@@ -1,34 +1,8 @@
 angular.module("budgetApp").controller("budgetCtrl", function($scope){
 
+    
 
-
-anychart.onDocumentLoad(function() {
-    // create an instance of a pie chart with data
-    var chart = anychart.line([
-      1,2,3,4,5]);
-
-    chart.container(angular.element(document.querySelector('#thecontainer')));
-    chart.draw();
-
-
-  });
 
   
 
 })
-
-// anychart.onDocumentLoad(function() {
-//     // create an instance of a pie chart with data
-//     var chart = anychart.pie([
-//       ["Chocolate", 5],
-//       ["Rhubarb compote", 2],
-//       ["Crêpe Suzette", 2],
-//       ["American blueberry", 2],
-//       ["Buttermilk", 1]
-//     ]);
-//     chart.title("Top 5 pancake fillings");
-//     // pass the container id, chart will be displayed there
-//     chart.container("container");
-//     // call the chart draw() method to initiate chart display
-//     chart.draw()
-//
